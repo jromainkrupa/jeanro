9b47d07116c9f05e9e9f5f4aad0e189cc48983e6
jeanromain KRUPA <jkrupa@hotmail.fr>
2021-02-23 22:38:51 +0100

    good old css to do

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [
+  './_includes/**/*.html',
+  './_layouts/**/*.html',
+  './_posts/*.md',
+  './*.html',],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
